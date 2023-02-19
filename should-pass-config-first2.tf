b420eb266a7e16fa3bfaefc54f6857419ca5a05c
ayala-orca <93713792+ayala-orca@users.noreply.github.com>
2023-02-19 10:19:58 +0200

    Update should-pass-config-first2.tf

--- should-pass-config-first2.tf
+++ should-pass-config-first2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "negative2" {
+resource "aws_instance" "configfirst" {
   ami = "ami-003634241a8fcdec0"
 
   instance_type = "t2.micro"
 
-  subnet_id   = aws_subnet.my_subnet2.id
+  subnet_id   = aws_subnet.configfirst.id
 
 }
