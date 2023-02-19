b15e0b42e635fae7d2516f8a33eaf789a5b66253
ayala-orca <93713792+ayala-orca@users.noreply.github.com>
2023-02-19 10:20:29 +0200

    Update should-pass-resource-first1.tf

--- should-pass-resource-first1.tf
+++ should-pass-resource-first1.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "negative3" {
+resource "aws_instance" "resourcefirst" {
   ami = "ami-003634241a8fcdec0"
 
   instance_type = "t2.micro"
 
-  subnet_id   = aws_subnet.my_subnet3.id
+  subnet_id   = aws_subnet.resourcefirst.id
 
 }
